7abd10d0e7e400c35a7d1c40f8e4e21704b40370
Surajit Das <dassurai@gmail.com>
2025-06-25 01:05:26 +0530

    strapi update 25625

---
+++ src/api/associate/controllers/associate.ts
@@ -0,0 +1,7 @@
+/**
+ * associate controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::associate.associate');
---
+++ src/api/associate/routes/associate.ts
@@ -0,0 +1,7 @@
+/**
+ * associate router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::associate.associate');
